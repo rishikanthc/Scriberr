99950093852e686ac92057b028336d3d4ef95768
Phillip Ferentinos <phillipjf@users.noreply.github.com>
2024-10-07 12:49:59 -0500

    feat(summarize-server): add configuration items to allow for setting alternative openai endpoints

--- src/routes/api/summarize/+server.ts
+++ src/routes/api/summarize/+server.ts
@@ -2,8 +2,12 @@ import OpenAI from 'openai';
 import type { RequestHandler } from '@sveltejs/kit';
 import { json } from '@sveltejs/kit';
 import { OPENAI_API_KEY } from '$env/static/private';
+import { OPENAI_ENDPOINT } from '$env/static/private';
+import { OPENAI_MODEL } from '$env/static/private';
+import { OPENAI_ROLE } from '$env/static/private';
 
 const openai = new OpenAI({
+	baseURL: OPENAI_ENDPOINT,
 	apiKey: OPENAI_API_KEY
 });
 
@@ -16,9 +20,11 @@ export const POST: RequestHandler = async ({ request, locals, fetch }) => {
 	const prompt = templateRecord.prompt;
 	const fullPrompt = `${transcript}\n${prompt}`;
 	const completion = await openai.chat.completions.create({
-		messages: [{ role: 'system', content: fullPrompt }],
-		model: 'gpt-4'
+		messages: [{ role: OPENAI_ROLE, content: fullPrompt }],
+		model: OPENAI_MODEL
 	});
+
+	console.log(completion)
 
 	locals.pb.collection('scribo').update(id, {
 		summary: completion.choices[0].message.content
