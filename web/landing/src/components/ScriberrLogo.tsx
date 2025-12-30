1d003ad00abd026d4fb149391183f54896bd4df0
rishikanthc <rishikanthc@gmail.com>
2025-12-30 12:20:06 -0800

    forgot to commit removal of old project site

--- web/landing/src/components/ScriberrLogo.tsx
+++
@@ -1,9 +0,0 @@
-export function ScriberrLogo({ className = '' }: { className?: string }) {
-  return (
-    <img
-      src="/scriberr-logo.png"
-      alt="Scriberr"
-      className={`w-auto select-none ${className}`}
-    />
-  );
-}
